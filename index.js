53c3b5ca95bc7718505af65290935c9aec253414
unknown <you@example.com>
2020-08-15 01:31:42 +0530

    css

---
+++ index.js
@@ -0,0 +1,65 @@
+const express=require("express");
+const app=express();
+const port=process.env.PORT || 8000;
+const mongoose=require("./config/mongoose");
+const router = require("./routes");
+const ejs=require("ejs");
+const path=require("path");
+const passport = require('passport');
+const session=require("express-session");
+var cookieParser = require('cookie-parser');
+require("./config/passport-local");
+const mongo_connect=require("connect-mongo")(session);
+app.use(express.urlencoded());
+
+
+app.use(cookieParser());
+app.use(session({
+   name:"codeial",
+   secret:"ptwmjg.ad",
+   saveUninitialized:false,
+   resave:false,
+   cookie:{
+     maxAge:(1000*60*60*24*2)
+   },
+   store:new mongo_connect({
+
+     mongooseConnection:mongoose,
+     autoRemove:"disable"
+
+   },function(err){
+     console.log(err||"connect-mongo")
+   })
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.isUserAuth);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"./views"));
+app.use(express.static('assets'));
+app.use("/forgotPass",express.static("assets"))
+
+
+
+app.listen(port,function(){
+     
+  console.log(`Server Running on ${port}`);
+})
+app.use(router);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
