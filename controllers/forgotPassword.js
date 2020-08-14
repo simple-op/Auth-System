73bc029ff1b994b8b66cfe7dd0314b00e461be36
unknown <you@example.com>
2020-08-15 02:19:07 +0530

    forgot password implemented

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -1,8 +1,16 @@
 const random=require("randomstring");
 const model=require("../models/token");
-var nodemailer = require('nodemailer');
+const user=require("../models/user")
+const nodemailer = require('nodemailer');
+const bcrypt=require("bcrypt");
+
 
 module.exports.forgot=function(req,res){
+               user.findOne({email:req.body.email},function(err,user){
+
+    
+ 
+              if(user){                
               let rtoken=random.generate();
               let email=req.body.email;
               model.findOne({email:email},function(err,token){
@@ -23,7 +31,7 @@ module.exports.forgot=function(req,res){
                     created:Date.now()
                 })
 
-                var transporter = nodemailer.createTransport({
+                let transporter = nodemailer.createTransport({
                     host: "smtp.ethereal.email",
                     port: 587,
                     secure: false,  
@@ -37,7 +45,7 @@ module.exports.forgot=function(req,res){
                   }
                 });
                 
-                var mailOptions = {
+                let mailOptions = {
                   from: 'rameshkumar160196@gmail.com',
                   to: email,
                   subject: 'Sending Email using Node.js',
@@ -69,16 +77,81 @@ module.exports.forgot=function(req,res){
 
             
               
-
-
+            }
+            })
             res.redirect("back");
+
+
              
 
 }
 
 
 module.exports.resetForgot=function(req,res){
+     
+    model.findOne({token:req.query.token},function(err,token){
 
-   res.render("forgotPass")
+        if(token&&(Date.now()-token.created)<60000){
+            res.render("forgotPass",{
+                token:token.token})
+                   
+        }
+        else{
+            res.redirect("/")
+        }  
+
+    })
+
+
+
+//     if(!token||(Date.now()-token.created)>60000)           
+
+
+//    res.render("forgotPass")
+
+}
+
+
+
+module.exports.resetForgotPass=function(req,res){
+
+    model.findOne({token:req.body.token},function(err,token){
+
+        if(token&&(Date.now()-token.created)<60000){
+            
+            user.findOne({email:token.email},function(err,userf){
+
+                function validatePassword(password) {
+                    const re = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})$/;
+                    return re.test(password);
+                    
+                }
+               if(validatePassword(req.body.password))
+                {
+                          if(req.body.re_password===req.body.password)
+                           {             bcrypt.hash(req.body.password, 10, function(err, hash){
+                                         user.findByIdAndUpdate(userf._id,{ password:hash },function(err,user){
+                                          
+                                            console.log("pass changed")
+                                            
+                                                 
+                            })
+        
+                           })
+                                    
+                           }
+
+ 
+            }
+                   
+        })
+    }
+        else{
+          return  res.redirect("/");
+        } 
+        return  res.redirect("/");
+    })
+
+  
 
 }
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get("/logout",require("../controllers/logout").logout);
 router.post("/resetPassword",require("../controllers/resetPassword").reset)
 router.post("/forgot",require("../controllers/forgotPassword").forgot)
 router.get("/forgotPass",require("../controllers/forgotPassword").resetForgot)
+router.post("/resetForgotPass",require("../controllers/forgotPassword").resetForgotPass)
+
 
 
 
