72d04022dfd8ade308a333541e90e0b0c06ddfe3
unknown <you@example.com>
2020-08-16 15:41:21 +0530

    everthg done

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -24,7 +24,7 @@ module.exports.forgot=function(req,res){
                     if(token){
                         console.log(Date.now())
                         
-                       
+                        
                         model.findByIdAndDelete(token._id, function(err){
 
                         })
